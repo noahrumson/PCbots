20b434af07bf8a45a25f356b3d437f5872dbb2ae
Peter Huson <peter@huson.com>
2018-03-10 14:18:02 -0500

    Init script to start 4 lidars

---
+++ LidarStuff/init/init_i2c.py
@@ -0,0 +1,53 @@
+import RPi.GPIO as GPIO  
+import subprocess 
+from time import sleep     # this lets us have a time delay: sleep(0.1)  
+
+GPIO.setmode(GPIO.BCM)     # Number GPIOs by channelID   
+GPIO.setwarnings(False)    # Ignore Errors
+
+# Setup all GPIO pins as low.
+GPIO.setup(6, GPIO.OUT)     # GPIO_06 = Lidar0
+GPIO.setup(13, GPIO.OUT)     # GPIO_13 = Lidar1
+GPIO.setup(19, GPIO.OUT)     # GPIO_19 = Lidar2
+GPIO.setup(26, GPIO.OUT)     # GPIO_26 = Lidar3
+
+try: 
+    print("CE Lidar0")
+
+    GPIO.output(6, 1) # Chip Enable by pulling high
+    
+    lidar0_process = subprocess.Popen(['./runme0'], stdout = subprocess.PIPE) 
+    std_output = lidar0_process.communicate()[0] 
+    
+    print("Output: " + std_output) # Print Output
+
+    print("CE Lidar1")
+
+    GPIO.output(13, 1) # Chip Enable by pulling high
+    
+    lidar0_process = subprocess.Popen(['./runme1'], stdout = subprocess.PIPE) 
+    std_output = lidar0_process.communicate()[0] 
+    
+    print("Output: " + std_output) # Print Output
+
+    print("CE Lidar0")
+
+    GPIO.output(19, 1) # Chip Enable by pulling high
+    
+    lidar0_process = subprocess.Popen(['./runme2'], stdout = subprocess.PIPE) 
+    std_output = lidar0_process.communicate()[0] 
+    
+    print("Output: " + std_output) # Print Output
+    
+    print("CE Lidar0")
+
+    GPIO.output(26, 1) # Chip Enable by pulling high
+    
+    lidar0_process = subprocess.Popen(['./runme3'], stdout = subprocess.PIPE) 
+    std_output = lidar0_process.communicate()[0] 
+    
+    print("Output: " + std_output) # Print Output
+
+finally: # If Ctr+C is pressed
+
+    print("Exiting")
