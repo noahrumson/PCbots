8849722257dec8aaa9af5d8d56d660c72f236a22
noahrumson <noahperryrubin@gmail.com>
2018-03-23 09:54:46 -0400

    Add files via upload

--- rpmtest.cpp
+++ rpmtest.cpp
@@ -2,56 +2,79 @@
 
 #include "pigpio.h"
 
-double cycle_min = 2.9;
-double cycle_max = 97.1;
+double cycle_min = 0.029;
+double cycle_max = 0.971;
+int feedback_period = 990;//1e6 / 910; 990 works better for some reason
 
-int on_time = 0.0;
-int total_time = 0.0;
+int last_pulse_width;
 void state_changed(int gpio, int level, uint32_t tick)
 {
 	static uint32_t lastTick = 0;
 	if (lastTick == 0) {
 		lastTick = tick;
 	}
-	uint32_t dt = tick - lastTick;
 	if (level == 0) {
-		on_time += dt;
+		last_pulse_width = tick - lastTick;
 	}
-	total_time += dt;
 	lastTick = tick;
 }
 
-double timer_dt = 1.0 / 100.0;
-void timer_check()
+double clamp(double x, double min, double max) 
 {
-	int state = gpioRead(27);
-	if (state) {
-		on_time += 1000 * timer_dt;
-	}
-	total_time += 1000 * timer_dt;
-	if (total_time > 1000 && total_time < 1200) {
-		double duty_cycle = 100.0 * (double)on_time / total_time;
-		double revolutions = (duty_cycle - cycle_min) / (cycle_max - cycle_min + 1.0);
-		std::cout << "duty cycle: " << duty_cycle << std::endl;
-		std::cout << "angle: " << revolutions << std::endl;
-		total_time = 0.0;
-		on_time = 0.0;
-	}
+	if (x < min)
+		return min;
+	if (x > max)
+		return max;
+	return x;
 }
 
-int main()
+/*
+	plug in wire with one end in port 27 and other in port 22
+*/
+void test_read_pi_pwm()
 {
-	double rpm1 = 6;
-	//std::cin >> rpm1;
+	double dcycle_out;
+	std::cin >> dcycle_out;
+	gpioInitialise();
+	gpioSetMode(27, PI_OUTPUT);
+	gpioSetMode(22, PI_INPUT);
+	gpioSetAlertFunc(22, state_changed);
+	gpioSetPWMfrequency(27, 910);
+	gpioPWM(27, 255 * dcycle_out);
+	gpioDelay(1e6);
+	gpioPWM(27, 0);
+
+	double duty_cycle = (double)last_pulse_width / feedback_period;
+	std::cout << "duty cycle: " << duty_cycle << std::endl;
+}
+
+void test_read_servo_feedback()
+{
+	int pulse_width;
+	do {
+		std::cin >> pulse_width;
+	} while (pulse_width > 1700 || pulse_width < 1500);
+
 	gpioInitialise();
 	gpioSetMode(4, PI_OUTPUT);
 	gpioSetMode(27, PI_INPUT);
 	gpioSetAlertFunc(27, state_changed);
-//	gpioSetTimerFunc(0, 1000 * timer_dt, timer_check);
-	gpioPWM(4, rpm1);
 	gpioSetPWMfrequency(4, 50);
+	gpioServo(4, pulse_width);
 	gpioDelay(1e6);
 	gpioPWM(4, 0);
 
+	double duty_cycle = (double)last_pulse_width / feedback_period;
+	duty_cycle = clamp(duty_cycle, cycle_min, cycle_max);
+	std::cout << "duty cycle: " << duty_cycle << std::endl;
+
+	double angle = (duty_cycle - cycle_min) / (cycle_max - cycle_min);
+	std::cout << "revolutions: " << angle << std::endl;
+}
+
+int main()
+{
+//	test_read_pi_pwm();
+	test_read_servo_feedback();
 	return 0;
 }
