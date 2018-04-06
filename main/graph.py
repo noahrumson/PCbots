b608226858a2be6cf6acfd7f0c25ef0323e1be41
cbear99 <camdenbaer@gmail.com>
2018-04-06 19:28:39 -0400

    updated bfs

--- main/graph.py
+++ main/graph.py
@@ -16,9 +16,17 @@ class Graph(object):
 		#create all initial nodes
 		self.nodeTable = {}
 		for i in range(self.size):
-			self.nodeTable[i] = new Node(i)
+			self.nodeTable[i] = Node(i)
 
 
+	#method to set currentNode
+	def setCurrentNode(self,nodeX,nodeY):
+		node = self.findNode(node1x,node1y)
+		self.currentNode = node
+
+	#method to update the amount of time it has been visited
+	def updateNodeVisit(self):
+		self.currentNode.visit()
 
 	#method to add an edge
 	def addEdge(self,node1x, node1y, node2x, node2y):
@@ -28,15 +36,16 @@ class Graph(object):
 		node2 = self.findNode(node2x, node2y)
 
 		#tests to see if nodes are even on the board
-		if node1 < 0 or node1 > self.size-1 or node2 < 0 or node 2 > self.size-1:
+		if node1.getIntVal() < 0 or node1.getIntVal() > self.size-1 or node2.getIntVal() < 0 or node2.getIntVal() > self.size-1:
 			print "Invalid node input, not on board"
+			return
 
 		#the differences between the inputted x and y valuse
 		xdif = node1x - node2x
 		ydif = node1y - node2y
 
 		#checking if the differences make sense, absval of xdif has to be 1 and ydif has to be 0, or xdif = 0 and absval of ydif has to be 1
-		elif (abs(xdif) = 1 and ydif = 0) or (abs(ydif) = 1 and xdif = 0):
+		if (abs(xdif) == 1 and ydif == 0) or (abs(ydif) == 1 and xdif == 0):
 			self.adjacencyArray[node1][node2] = 1
 			self.adjacencyArray[node2][node1] = 1
 
@@ -50,7 +59,7 @@ class Graph(object):
 		index = node.getIntVal()
 
 		for i in range(self.boardWidth):
-			if self.adjacencyArray[index][i] = 1:
+			if self.adjacencyArray[index][i] == 1:
 				adjacent.append(i)
 
 		return adjacent
@@ -72,7 +81,7 @@ class Graph(object):
 	def isConnected(self, node1x, node1y, node2x, node2y):
 		node1 = self.findNode(node1x,node1y)
 		node2 = self.findNode(node2x, node2y)
-		if self.adjacencyArray[node1][node2] = 1 or self.adjacencyArray[node2][node1] = 1:
+		if self.adjacencyArray[node1][node2] == 1 or self.adjacencyArray[node2][node1] == 1:
 			return True
 		else:
 			return False
@@ -87,25 +96,24 @@ class Graph(object):
 		queue.append(node1)
 
 
-		while not len(queue) = 0:
+		while not len(queue) == 0:
 
 			#pop the first thing in queue and gets neigbors
 			currentNode = queue.popleft()
 			neighbors = self.findAdjacent(currentNode)
 
 			#gets us out of the loop if it finds desired nodes
-			if currentNode = node2:
+			if currentNode == node2:
 				break
 
 			#for every neigbor, if it hasn;t been marked yet, mark it with a prevNode and append it to the queue
 			for i in range(neigbors):
-
-				if not neigbors[i].getPrev = None:
+				if neigbors[i].getPrev == None:
 					neigbors[i].setPrev(currentNode)
 					queue.append(neigbors[i])
 
 		#if it traversed all paths and didn't find it
-		if not currentNode = node2:
+		if not currentNode == node2:
 			return None
 
 		#shortest path begins with endpoint	
@@ -113,21 +121,22 @@ class Graph(object):
 		shortestPath.appendLeft(currentNode)
 
 		#then go back through the prev pointers adding the 
-		while not currentNode.getPrev() = None:
+		while not currentNode.getPrev() == None:
 			currentNode = currentNode.getPrev()
 			shortestPath.appendLeft(currentNode)
 
-		return shortestPath
+		shortestPathBack = list(shortestPath)
+		shortestPathBack.reverse()
+		return (shortestPath,shortestPathBack)
 
 	#returns the robot's current node		
 	def getCurrentNode(self):
 		return self.currentNode
 
 
-class Node(object, intVal):
-
+class Node(object):
 	#constructor
-	def __init__(self):
+	def __init__(self,intVal):
 		self.intVal = intVal
 		self.prevNode = None
 		self.visited = 0
@@ -151,6 +160,12 @@ class Node(object, intVal):
 	#accessor for visited
 	def getVisited(self):
 		return self.visited
+
+	def getXY(self):
+		nodeY = intVal/self.boardWidth
+		nodeX = intVal - self.boardWidth*nodeY
+
+		return(nodeX,nodeY)
 
 
 
