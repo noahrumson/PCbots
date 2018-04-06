456e23a18b359afaa8bbcf61502ddc158c7cd1ca
cbear99 <camdenbaer@gmail.com>
2018-04-06 11:57:26 -0400

    updated bfs

--- main/graph.py
+++ main/graph.py
@@ -8,20 +8,27 @@ class Graph(object):
 		#declare instance variables
 		self.size = 256
 		self.boardWidth = 16
+		self.currentNode = 0
 
 		#creating the initial 2D array set to all 0s, meaning no connections between the edges
-		adjacencyArray = [[0 for x in range(size)] for y in range(size)]
+		self.adjacencyArray = [[0 for x in range(self.size)] for y in range(self.size)]
+
+		#create all initial nodes
+		self.nodeTable = {}
+		for i in range(self.size):
+			self.nodeTable[i] = new Node(i)
+
 
 
 	#method to add an edge
 	def addEdge(self,node1x, node1y, node2x, node2y):
 
 		#convert the xy pos to actual positions 1 - 255 for the adjacency matrix
-		node1 = findNode(node1x, node1y)
-		node2 = findNode(node2x, node2y)
+		node1 = self.findNode(node1x, node1y)
+		node2 = self.findNode(node2x, node2y)
 
 		#tests to see if nodes are even on the board
-		if node1 < 0 or node1 > size-1 or node2 < 0 or node 2 > size-1:
+		if node1 < 0 or node1 > self.size-1 or node2 < 0 or node 2 > self.size-1:
 			print "Invalid node input, not on board"
 
 		#the differences between the inputted x and y valuse
@@ -30,42 +37,42 @@ class Graph(object):
 
 		#checking if the differences make sense, absval of xdif has to be 1 and ydif has to be 0, or xdif = 0 and absval of ydif has to be 1
 		elif (abs(xdif) = 1 and ydif = 0) or (abs(ydif) = 1 and xdif = 0):
-			adjacencyArray[node1][node2] = 1
-			adjacencyArray[node2][node1] = 1
+			self.adjacencyArray[node1][node2] = 1
+			self.adjacencyArray[node2][node1] = 1
 
 		else:
 			print "Invalid node inputs, nodes should not be adjacent"
 
 	#method to find adjacent vertices to index
-	def findAdjacent(self,nodeX, nodeY):
-
-		#converting xy pos to actual position
-		node = nodeX + boardWidth*nodeY
+	def findAdjacent(self,node):
 
 		adjacent = []
-		for i in range(size):
-			if adjacencyArray[index][i] = 1:
+		index = node.getIntVal()
+
+		for i in range(self.boardWidth):
+			if self.adjacencyArray[index][i] = 1:
 				adjacent.append(i)
 
 		return adjacent
 
 	#method to convert xy pairs to node
 	def findNode(self,nodeX,nodeY):
-		node = nodeX + boardWidth*nodeY
+		key = nodeX + self.boardWidth*nodeY
+		return self.nodeTable[key]
 
 	#method to convert nodes to the xy pairs
-	def findXY(self,node):
+	def findXY(self, node):
 
-		nodeY = node/boardWidth
-		nodeX = node - boardWidth*nodeY
+		nodeY = node.getIntVal()/self.boardWidth
+		nodeX = node.getIntVal() - self.boardWidth*nodeY
 
 		return(nodeX,nodeY)
 
 	#method to find if two nodes are connected
 	def isConnected(self, node1x, node1y, node2x, node2y):
-		node1 = findNode(node1x,node1y)
-		node2 = findNode(node2x, node2y)
-		if adjacencyArray[node1][node2] = 1 or adjacencyArray[node2][node1] = 1:
+		node1 = self.findNode(node1x,node1y)
+		node2 = self.findNode(node2x, node2y)
+		if self.adjacencyArray[node1][node2] = 1 or self.adjacencyArray[node2][node1] = 1:
 			return True
 		else:
 			return False
@@ -73,14 +80,77 @@ class Graph(object):
 	#returns a list of the shortest path between two points
 	def shortestPath(self, node1x, node1y, node2x, node2y):
 
-		node1 = findNode(node1x, node1y)
-		node2 = findNode(node2x, node2y)
+		node1 = self.findNode(node1x, node1y)
+		node2 = self.findNode(node2x, node2y)
 
 		queue = deque()
 		queue.append(node1)
 
+
 		while not len(queue) = 0:
+
+			#pop the first thing in queue and gets neigbors
 			currentNode = queue.popleft()
+			neighbors = self.findAdjacent(currentNode)
+
+			#gets us out of the loop if it finds desired nodes
+			if currentNode = node2:
+				break
+
+			#for every neigbor, if it hasn;t been marked yet, mark it with a prevNode and append it to the queue
+			for i in range(neigbors):
+
+				if not neigbors[i].getPrev = None:
+					neigbors[i].setPrev(currentNode)
+					queue.append(neigbors[i])
+
+		#if it traversed all paths and didn't find it
+		if not currentNode = node2:
+			return None
+
+		#shortest path begins with endpoint	
+		shortestPath = deque()
+		shortestPath.appendLeft(currentNode)
+
+		#then go back through the prev pointers adding the 
+		while not currentNode.getPrev() = None:
+			currentNode = currentNode.getPrev()
+			shortestPath.appendLeft(currentNode)
+
+		return shortestPath
+
+	#returns the robot's current node		
+	def getCurrentNode(self):
+		return self.currentNode
+
+
+class Node(object, intVal):
+
+	#constructor
+	def __init__(self):
+		self.intVal = intVal
+		self.prevNode = None
+		self.visited = 0
+
+	#mutator for the previous node used in bfs algorithm
+	def setPrev(self, Node):
+		self.prevNode = Node
+
+	#accessor for the previous node
+	def getPrev(self):
+		return self.prevNode
+
+	#accessor for the intval
+	def getIntVal(self):
+		return self.intVal
+
+	#increment the amount its been visited
+	def visit(self):
+		self.visited += 1
+
+	#accessor for visited
+	def getVisited(self):
+		return self.visited
 
 
 
